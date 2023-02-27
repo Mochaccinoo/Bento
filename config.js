ddef0a0d6ee6b7d91b326c63c5a3eb90513b65b2
Mocha <71618230+Mochaccinoo@users.noreply.github.com>
2023-02-27 14:17:38 -0500

    Update config.js

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ const CONFIG = {
   	title: 'New Tab',
 
 	// Theme
-	theme: ' latte-macchiato',
+	theme: 'latte-mocha',
 	imageBackground: false,
   imageUrl: './assets/background.jpg', // Set custom background image URL. If the page is served insecurely, you may have issues loading images from pages over https.
 
